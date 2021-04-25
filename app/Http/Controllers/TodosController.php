37bd1fc73f0fa1a0c67d1b7c102d16d1695082f7
ik-code <igo.metacon@gmail.com>
2021-04-25 21:08:43 +0300

    model view controller

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todos;
 use Illuminate\Http\Request;
 
-class TodosController extends Controller
-{
-    public function index(){
-return view('todos.index');
+class TodosController extends Controller {
+    public function index() {
+        return view( 'todos.index' )->with( 'todos', Todos::all() );
+    }
+    public function show($todo_id) {
+        $todo = Todos::find($todo_id);
+        return view('todos.show')->with('todo', $todo);
     }
 }
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
-    <title>Todos</title>
+    <title>Todo Item</title>
 </head>
 <body>
 <div class="container">
@@ -14,12 +14,14 @@
         <div class="col-lg-6">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="text-center m-5">Todo list</h3>
+                    <h3 class="text-center m-5">{{$todo->name}} </h3>
                 </div>
                 <ul class="list-group list-group-flush">
-                    @foreach( $todos as $todo)
-                        <li class="list-group-item">{{$todo->name}} <a href="/todos/{{ $todo->id }}" type="button" class="btn btn-primary btn-sm float-right">View</a></li>
-                    @endforeach
+                    <li class="list-group-item"><h5 class="text-center">Details</h5></li>
+                    <li class="list-group-item">{{ $todo->description }}</li>
+                    <li class="list-group-item text-center"><a href="/todos" type="button"
+                                                   class="btn btn-primary btn-sm">Go Back</a></li>
+
                 </ul>
             </div>
         </div>
