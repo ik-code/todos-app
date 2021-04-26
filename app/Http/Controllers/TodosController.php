21505e3b7a86954f757353584817f1851b73bc4e
ik-code <igo.metacon@gmail.com>
2021-04-26 18:08:31 +0300

    model binding

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -9,8 +9,7 @@ class TodosController extends Controller {
     public function index() {
         return view( 'todos.index' )->with( 'todos', Todos::all() );
     }
-    public function show($todo_id) {
-        $todo = Todos::find($todo_id);
+    public function show(Todos $todo) {
         return view('todos.show')->with('todo', $todo);
     }
     public function create(){
@@ -29,25 +28,23 @@ class TodosController extends Controller {
         $todo->save();
         return redirect('/todos');
     }
-    public function edit($todo_id){
-        $todo = Todos::find($todo_id);
+    public function edit(Todos $todo){
         return view('todos.edit')->with('todo',  $todo);
     }
-    public function update($todo_id){
+    public function update(Todos $todo){
         $this->validate( request(), [
-            'name' => 'required|unique:todos|max:255',
+            'name' => 'required|max:255',
             'description' => 'required'
         ]);
         $data = request()->all();
-        $todo = Todos::find($todo_id);
+
         $todo->name = $data['name'];
         $todo->description = $data['description'];
 
         $todo->save();
         return redirect('/todos');
     }
-    public function destroy($todo_id){
-        $todo = Todos::find($todo_id);
+    public function destroy(Todos $todo){
         $todo->delete();
         return redirect('/todos');
     }
