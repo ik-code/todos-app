67c8ee33ecc0237c70cc393ea6964b177e3b5491
ik-code <igo.metacon@gmail.com>
2021-04-26 10:05:03 +0300

    layouts

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,15 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
-    <title>Todos</title>
-</head>
-<body>
-<div class="container">
+@extends('layouts.app')
+@section('title')
+    Todos
+    @endsection
+@section('content')
     <div class="row justify-content-center">
         <div class="col-lg-6">
             <div class="card">
@@ -24,7 +17,4 @@
             </div>
         </div>
     </div>
-</div>
-
-</body>
-</html>
+@endsection
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,15 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
-    <title>Todo Item</title>
-</head>
-<body>
-<div class="container">
+@extends('layouts.app')
+@section('title')
+    {{$todo->name}}
+    @endsection
+@section('content')
     <div class="row justify-content-center">
         <div class="col-lg-6">
             <div class="card">
@@ -21,12 +14,8 @@
                     <li class="list-group-item">{{ $todo->description }}</li>
                     <li class="list-group-item text-center"><a href="/todos" type="button"
                                                    class="btn btn-primary btn-sm">Go Back</a></li>
-
                 </ul>
             </div>
         </div>
     </div>
-</div>
-
-</body>
-</html>
+@endsection
